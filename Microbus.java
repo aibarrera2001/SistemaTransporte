8e53c2b368785bfdd957e1422efbc044a4476519
jpablomontero-byte <jpablomontero@unicesar.edu.co>
2026-03-18 16:00:30 -0500

    Update Microbus.java
    Updated parameter descriptions in the Microbus constructor.

--- Microbus.java
+++ Microbus.java
@@ -7,9 +7,9 @@ public class Microbus extends Vehiculo {
     private static final int TARIFA    = 6000;
 
     /**
-     * @param placa      Placa del vehículo.
-     * @param ruta       Objeto {@link Ruta} obtenido del catálogo (no puede ser null).
-     * @param disponible Disponibilidad del vehículo.
+     * @param placa     
+     * @param ruta      
+     * @param disponible 
      */
     public Microbus(String placa, Ruta ruta, boolean disponible) {
         super(placa, ruta, disponible);
