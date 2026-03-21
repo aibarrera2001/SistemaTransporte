1d207e24e93a24bdcbb2e15607b597afdbabcbdd
Jose Luis Barreto <barretoluisjose06@gmail.com>
2026-03-21 00:41:59 -0500

    feat(model): agregar interfaces Imprimible y Calculable

--- src/main/java/com/mycompany/sistematransporte/model.java
+++ src/main/java/com/mycompany/sistematransporte/model.java
@@ -2,12 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package personaticket;
+package com.mycompany.sistematransporte;
 
 /**
  *
  * @author jose
  */
-public class ticket {
+public class model {
     
+    public interface Imprimible {
+    void imprimirDetalle();
+}
+    
+    public interface Calculable {
+    double calcularTotal();
+}
 }
--- src/main/java/com/mycompany/sistematransporte/ticket.java
+++ src/main/java/com/mycompany/sistematransporte/ticket.java
@@ -2,20 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package personaticket;
+package com.mycompany.sistematransporte;
 
 /**
  *
  * @author jose
  */
-public class model {
+public class ticket {
     
-
-public interface Imprimible {
-    void imprimirDetalle();
-}
-
-public interface Calculable {
-    double calcularTotal();
-}
 }
