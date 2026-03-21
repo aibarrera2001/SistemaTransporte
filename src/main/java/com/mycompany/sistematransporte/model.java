5cb7375860743773f878e7ac57ec616fc523ca20
Jose Luis Barreto <barretoluisjose06@gmail.com>
2026-03-21 00:46:26 -0500

    feat(model): agregar clase Conductor con validacion de licencia

--- src/main/java/com/mycompany/sistematransporte/model.java
+++ src/main/java/com/mycompany/sistematransporte/model.java
@@ -37,4 +37,25 @@ public abstract class Persona implements Imprimible {
         return nombre;
     }
 }
+
+public class Conductor extends Persona {
+
+    private String licencia;
+    private String categoria;
+
+    public Conductor(String cedula, String nombre, String licencia, String categoria) {
+        super(cedula, nombre);
+        this.licencia = licencia;
+        this.categoria = categoria;
+    }
+
+    public boolean tieneLicencia() {
+        return licencia != null && !licencia.isEmpty();
+    }
+
+    @Override
+    public void imprimirDetalle() {
+        System.out.println("Conductor: " + nombre + " - Licencia: " + licencia);
+    }
+}
 }
