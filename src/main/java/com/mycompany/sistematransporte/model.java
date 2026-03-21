74a20009a40be925b545254c295ef18192a110b9
Jose Luis Barreto <barretoluisjose06@gmail.com>
2026-03-21 00:47:36 -0500

    feat(model): implementar clase abstracta Pasajero

--- src/main/java/com/mycompany/sistematransporte/model.java
+++ src/main/java/com/mycompany/sistematransporte/model.java
@@ -58,4 +58,13 @@ public class Conductor extends Persona {
         System.out.println("Conductor: " + nombre + " - Licencia: " + licencia);
     }
 }
+
+public abstract class Pasajero extends Persona {
+
+    public Pasajero(String cedula, String nombre) {
+        super(cedula, nombre);
+    }
+
+    public abstract double calcularDescuento();
+}
 }
