a2c7229f8514d97919bfc62e7696b07aad0f2f6b
Jose Luis Barreto <barretoluisjose06@gmail.com>
2026-03-21 01:01:17 -0500

    feat(service): implementar registro de pasajeros y conductores con validaciones

--- src/main/java/com/mycompany/sistematransporte/ticket.java
+++ src/main/java/com/mycompany/sistematransporte/ticket.java
@@ -3,11 +3,33 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.sistematransporte;
-
+import dao.*;
+import model.*;
 /**
  *
  * @author jose
  */
 public class ticket {
-    
+
+public class PersonaService {
+
+    private PasajeroDAO pasajeroDAO = new PasajeroDAO();
+    private ConductorDAO conductorDAO = new ConductorDAO();
+
+    public void registrarPasajero(Pasajero pasajero) {
+        pasajeroDAO.guardar(pasajero);
+        System.out.println("✅ Pasajero registrado");
+    }
+
+    public void registrarConductor(Conductor conductor) {
+
+        if (!conductor.tieneLicencia()) {
+            System.out.println("❌ El conductor no tiene licencia");
+            return;
+        }
+
+        conductorDAO.guardar(conductor);
+        System.out.println("✅ Conductor registrado");
+    }
+}
 }
