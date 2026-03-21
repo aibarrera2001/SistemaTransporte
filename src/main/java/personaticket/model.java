f6e1ae246529a19aeb1b217818bcc1bfdb0ac400
Jose Luis Barreto <barretoluisjose06@gmail.com>
2026-03-21 00:25:31 -0500

    feat(model): agregar estructura inicial de model, dao y ticket

---
+++ src/main/java/personaticket/model.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personaticket;
+
+/**
+ *
+ * @author jose
+ */
+public class model {
+    
+
+public interface Imprimible {
+    void imprimirDetalle();
+}
+
+public interface Calculable {
+    double calcularTotal();
+}
+}
