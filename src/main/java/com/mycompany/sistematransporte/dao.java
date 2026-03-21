c6a97febd995f217999fca0ce875d599c24091a2
Jose Luis Barreto <barretoluisjose06@gmail.com>
2026-03-21 00:56:25 -0500

    feat(dao): implementar persistencia de pasajeros en archivo

--- src/main/java/com/mycompany/sistematransporte/dao.java
+++ src/main/java/com/mycompany/sistematransporte/dao.java
@@ -3,11 +3,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.sistematransporte;
+import model.Pasajero;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
  * @author jose
  */
 public class dao {
-    
+
+public class PasajeroDAO {
+
+    public void guardar(Pasajero pasajero) {
+        try (FileWriter fw = new FileWriter("pasajeros.txt", true)) {
+            fw.write(pasajero.getCedula() + ";" + pasajero.getNombre() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 }
