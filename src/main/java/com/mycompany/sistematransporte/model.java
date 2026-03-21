3981c368180e077e7aa1f6af7001ac49efcf8c20
Jose Luis Barreto <barretoluisjose06@gmail.com>
2026-03-21 00:44:53 -0500

    feat(model): implementar clase abstracta Persona

--- src/main/java/com/mycompany/sistematransporte/model.java
+++ src/main/java/com/mycompany/sistematransporte/model.java
@@ -17,4 +17,24 @@ public class model {
     public interface Calculable {
     double calcularTotal();
 }
+   
+
+public abstract class Persona implements Imprimible {
+
+    protected String cedula;
+    protected String nombre;
+
+    public Persona(String cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
 }
