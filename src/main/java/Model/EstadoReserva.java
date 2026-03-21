e73bca044eed939e8b1c87303f21e75e3aa9c878
Juan Montero <jpablomontero@unicesar.edu.co>
2026-03-21 11:26:55 -0500

    agregar Reserva y ReservaDAO

---
+++ src/main/java/Model/EstadoReserva.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.Model;
+
+/**
+ *
+ * @author USUARIO
+ */
+public enum EstadoReserva {
+    ACTIVA, CONVERTIDA, CANCELADA
+}
